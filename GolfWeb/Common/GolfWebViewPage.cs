08a2fb15d5ae770eee564a4d2132e031b8b63c03
juniud <wangjunbo924@gmail.com>
2013-02-02 22:48:16 +0800

    add custom web view base page and add configurable default title/keywords/description to the base page

---
+++ GolfWeb/Common/GolfWebViewPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace GolfWeb.Common
+{
+    public abstract class GolfWebViewPage<T> : WebViewPage<T>
+    {
+        /// <summary>
+        /// page title
+        /// </summary>
+        public virtual string PageTitle
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["DefaultPageTitle"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the page keywords.
+        /// </summary>
+        public string PageKeywords
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["DefaultKeywords"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the page description.
+        /// </summary>
+        public string PageDescription
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["DefaultDescription"];
+            }
+        }
+    }
+}
